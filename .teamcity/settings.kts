e29ccc11bb116eac7af0f7fa5559ccb068899fe2
artb1sh <artb1sh@TeamCity>
2022-01-11 20:17:34 +0000

    TeamCity change in 'test' project: runners of 'nodejs' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -51,7 +51,7 @@ object Nodejs : BuildType({
 
     steps {
         nodeJS {
-            shellScript = "npm ci"
+            shellScript = "npm i"
         }
         nodeJS {
             shellScript = """
